761f81d0b9721dbc9d1dc05061bcc38f21d91685
CarloFidel <carloftaboada@gmail.com>
2026-02-16 10:18:53 +0100

    feat: Implementation of Soldier class

--- src/Soldier.ts
+++ src/Soldier.ts
@@ -1,1 +1,25 @@
-export class Soldier {}
+export class Soldier {
+  health: number;
+  strength: number;
+
+  constructor(health: number, strength: number) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage: Soldiers["damage"]) {
+    this.health = this.health - damage;
+  }
+}
+
+type Soldiers = {
+  health: number;
+  strenght: number;
+  attack: Attack;
+  damage: number;
+};
+
+type Attack = () => Soldiers["strenght"];
